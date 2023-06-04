d8e7966ddf109b7347118e1bdb1754158188121c
thong.bui <thongbui97@gmail.com>
2023-06-05 00:19:10 +0700

    fix: update for release

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,6 @@
+declare const _default: {
+    register(capturedBackgroundColor: String | null, callback: (arg0: any) => void): void;
+    unregister(): void;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
