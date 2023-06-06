f6f7f200f16ee2a57911502689fe43e31c5764fb
thong.bui <thongbui97@gmail.com>
2023-06-06 14:15:41 +0700

    fix: fix missing index.js

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,5 +1,13 @@
 declare const _default: {
+    /**
+     * activate screenshot blocking (iOS13+, Android 5+)
+     * @param string capturedBackgroundColor (iOS only) background color layout after taking a screenshot
+     * @param void callback callback after a screenshot has been taken
+     */
     register(capturedBackgroundColor: String | null, callback: (arg0: any) => void): void;
+    /**
+     * Deactivate screenshot
+     */
     unregister(): void;
 };
 export default _default;
