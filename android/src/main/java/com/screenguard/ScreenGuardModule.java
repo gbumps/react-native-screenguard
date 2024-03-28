07a2281ec40849362d51b62c34f22656ae792768
thong.bui <thongbui97@gmail.com>
2024-03-28 23:21:41 +0700

    feat: update event after screenshot

--- android/src/main/java/com/screenguard/ScreenGuardModule.java
+++ android/src/main/java/com/screenguard/ScreenGuardModule.java
@@ -142,4 +142,36 @@ public class ScreenGuardModule extends ReactContextBaseJavaModule {
             e.printStackTrace();
         }
     }
+
+    private Bitmap captureReactView(View view) {
+        Bitmap bitmap = Bitmap.createBitmap(
+                view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        return bitmap;
+    }
+
+    private String saveBitmapToFile(Bitmap bitmap) {
+        try {
+            File imagePath = new File(
+                    getReactApplicationContext().getCacheDir(), "screenshot_image");
+            if (!imagePath.exists()) {
+                imagePath.mkdirs();
+            }
+            Calendar calendar = Calendar.getInstance();
+            long time = calendar.getTimeInMillis();
+
+            File imageFile = new File(imagePath, "screenshot_" + time + ".png");
+
+            FileOutputStream fos = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+
+            return imageFile.getAbsolutePath();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }    
 }
