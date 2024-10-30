93c345d32ff68981e8525b33fb9a9539201c4da5
Baptiste <hello@strack.dev>
2024-10-30 12:57:17 +0200

    fix: colorFromHexString failed parsing

--- ios/ScreenGuard.mm
+++ ios/ScreenGuard.mm
@@ -212,11 +212,21 @@ UIScrollView *scrollView;
 }
 
 - (UIColor *)colorFromHexString:(NSString *)hexString {
+    if (hexString.length != 7 || ![hexString hasPrefix:@"#"]) {
+        // Handle invalid format by returning a default color, e.g., white.
+        return [UIColor whiteColor];
+    }
     unsigned rgbValue = 0;
     NSScanner *scanner = [NSScanner scannerWithString:hexString];
     [scanner setScanLocation:1]; // bypass '#' character
-    [scanner scanHexInt:&rgbValue];
-    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+    if (![scanner scanHexInt:&rgbValue]) {
+        // Return a default color if parsing fails
+        return [UIColor whiteColor];
+    }
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0
+                           green:((rgbValue & 0xFF00) >> 8)/255.0
+                            blue:(rgbValue & 0xFF)/255.0
+                           alpha:1.0];
 }
 
 - (UIImage *)convertViewToImage:(UIView *)view {
