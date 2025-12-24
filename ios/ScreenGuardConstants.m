7215b402fdd992b68a7e38c67bda4113e9c588b1
thong.bui <thongbui97@gmail.com>
2025-12-25 01:32:46 +0700

    refactor: add constants

---
+++ ios/ScreenGuardConstants.m
@@ -0,0 +1,42 @@
+#import "ScreenGuardConstants.h"
+
+// Config Keys
+NSString *const kSGConfigEnableCapture = @"enableCapture";
+NSString *const kSGConfigEnableRecord = @"enableRecord";
+NSString *const kSGConfigEnableMultitask = @"enableContentMultitask";
+NSString *const kSGConfigDisplayOverlay = @"displayOverlay";
+NSString *const kSGConfigTimeAfterResume = @"timeAfterResume";
+NSString *const kSGConfigGetScreenshotPath = @"getScreenshotPath";
+NSString *const kSGConfigLimitCaptureEvtCount = @"limitCaptureEvtCount";
+NSString *const kSGConfigTrackingLog = @"trackingLog";
+
+// Log Actions
+NSString *const kSGActionInit = @"init";
+NSString *const kSGActionStateChange = @"state_change";
+NSString *const kSGActionScreenshotTaken = @"screenshot_taken";
+NSString *const kSGActionRecordingStart = @"recording_start";
+NSString *const kSGActionRecordingStop = @"recording_stop";
+NSString *const kSGActionRemoveShield = @"remove_shield";
+
+// Events
+NSString *const kSGEventScreenGuard = @"onScreenGuardEvt";
+NSString *const kSGEventScreenshot = @"onScreenShotCaptured";
+NSString *const kSGEventScreenRecording = @"onScreenRecordingCaptured";
+
+// Defaults
+NSString *const kSGMethodBlur = @"blur";
+NSString *const kSGMethodImage = @"image";
+NSString *const kSGMethodColor = @"color";
+
+// User Defaults
+NSString *const kSGUserDefaultsConfig = @"com.screenguard";
+NSString *const kSGUserDefaultsLogs = @"com.screenguard.logs";
+
+// Errors & Promises
+NSString *const kSGErrorDomain = @"ScreenGuard";
+NSString *const kSGErrorActivateShield = @"activateShield";
+NSString *const kSGErrorActivateShieldBlur = @"activateShieldWithBlurView";
+NSString *const kSGErrorActivateShieldImage = @"activateShieldWithImage";
+NSString *const kSGErrorDeactivateShield = @"deactivateShield";
+NSString *const kSGErrorActivateShieldNoEffect = @"activateShieldWithoutEffect";
+NSString *const kSGErrorInvalidParams = @"EINVAL";
